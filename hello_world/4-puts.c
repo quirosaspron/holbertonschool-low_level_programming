3e29a6eee571ec55c55c266d18a9a956dca44f21
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-02 10:58:43 -0500

    s

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
-int main()
+/**
+ * main - Print a string
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle \n");
-	return 0;
+	return (0);
 }
