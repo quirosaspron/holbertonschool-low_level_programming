6188d1536dcc7b8f61143ffa11a716c04983913d
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-03 10:28:41 -0500

    f

---
+++ hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - to print
+ *
+ * Return: Will be 0
+ * */
+
+int main(void)
+{
+	fwrite(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 61);
+	return (1);
+}
