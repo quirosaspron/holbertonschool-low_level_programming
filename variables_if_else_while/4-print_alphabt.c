446bf0013d1d3908354b7b58ca5677b47b2ae54d
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-03 16:43:40 -0500

    s

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+* something
+*/
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c == 'e' && c == 'q')
+			continue;
+
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
