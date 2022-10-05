4b58717c4ddc73fdcf7445d8ee5d8ed2bf0f7533
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-05 06:13:11 -0500

    s

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,21 +1,17 @@
 #include "main.h"
 /**
- * print_alphabet - prints the alphabet
+ * print_alphabet_x10 - prints the alphabet 10 times
  *
  * Return: On success 1.
  */
 
-void print_alphabet(void)
+void print_alphabet_x10(void)
 {
-	char i;
 	int a;
 
 	for (a = 1; a <= 10; a++)
 	{
-		for (i = 'a'; i <= 'z'; i++)
-		{
-			_putchar(i);
-		}
-	_putchar('\n');
+		print_alphabet();
+		_putchar('\n');
 	}
 }
