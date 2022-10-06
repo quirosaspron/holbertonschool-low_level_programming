425c9efaab946aa47808f46ffe24ba7b2389e410
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-06 18:56:04 -0500

    s

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ *
+ * @n: The number to get its last digit
+ *
+ * Return: The last digit of a number
+ */
+
+int print_last_digit(int n)
+{
+	int r = n % 10;
+
+	_putchar(r);
+}
