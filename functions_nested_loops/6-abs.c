118c3b3e209e1ca0350bf4fe6ff293d5dde8266d
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-06 08:42:45 -0500

    s

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - gives you the absolute value of a number
+ *
+ * @n: The number to get its absolute value
+ *
+ * Return: the absolute value of the number
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return  (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
