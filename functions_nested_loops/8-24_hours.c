3dc0fa1fcf191c705cae78b391420bcf773a8610
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-07 07:28:20 -0500

    s

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/*
+ * jack_bauer - print the minutes of the day
+ * Return: void
+ */
+
+void jack_bauer(void);
+{
+	for (h = 0; h <= 23; h++)
+		{
+			for (m = 0; m <= 59; m++)
+			{
+				_putchar((h / 10) + '0');
+				_putchar((h % 10 ) + '0');
+				_putchar(':');
+				_putchar((m / 10) +'0');
+				_putchar(m % 10) +'0');
+				_putchar('\n');
+			}
+		}
+}
