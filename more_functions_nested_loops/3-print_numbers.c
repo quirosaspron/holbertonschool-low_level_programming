fb0676b16218f91b212c677d996a7e6b04d64c22
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-09 17:56:16 -0500

    s

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers from 0 to 9
+ *
+ */
+
+void print_numbers(void)
+{
+	for (int i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_puthcar('\n');
+}
