a83fa29650b2948fe511f935966aeda52983295f
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-08 21:18:23 -0500

    s

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -11,18 +11,18 @@ void print_to_98(int n)
 {
 	if (n < 98)
 	{
-		for (n = n; n <= 98; n++)
+		for (n = n; n <= 97; n++)
 		{
 			printf("%d,", n);
 		}
-	printf("\n");
+	printf("98 \n");
 	}
 	else
 	{
-		for (n = n; n >= 98; n--)
+		for (n = n; n >= 97; n--)
 		{
 			printf("%d,", n);
 		}
-	printf("\n");
+	printf("98 \n");
 	}
 }
