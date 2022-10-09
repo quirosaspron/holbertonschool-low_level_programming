e2441cfd04bdce99f8295449ed9d380c893d9759
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-09 17:47:25 -0500

    s

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two numbers together
+ *
+ * @a: first number to multiply
+ *
+ * @b: second number to multiply
+ *
+ * Return: multiplication between a and b
+ *
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
