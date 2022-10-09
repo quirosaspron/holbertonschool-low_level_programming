62261529afaa39a9b1f33c1d6e8ac831e9796a27
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-08 21:05:06 -0500

    s

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - a function that prints all number from a starting number to 98
+ *
+ * @n: the starting number
+ *
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		for (n; n == 98; n++)
+		{
+			printf("%d,", n)
+		}
+	}
+	else
+	{
+		for (n; n == 98; n--)
+		{
+			printf("%d,", n)
+		}
+	}
+}
