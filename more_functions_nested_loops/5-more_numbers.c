61c9a8a9608bb4c4a1ebb443fe96e46a4eb58c72
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-10 18:37:30 -0500

    s

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers from 0 to 14 ten times
+ *
+ */
+
+void more_numbers(void)
+{
+	int i = 0;
+	int b = 0;
+
+	while (b <= 9)
+	{
+		while (a <= 14)
+		{
+			if (a > 9)
+				_putchar(a / 10 + '0');
+				_putchar(a % 10 + '0');
+				a++;
+
+		}
+		putchar('\n');
+		b++;
+		a = 0;
+	}
+}
