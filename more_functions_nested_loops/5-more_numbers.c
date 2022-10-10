9eca156482dc6f4718742073e7481a6e749b2d5b
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-10 18:52:18 -0500

    s

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,14 +10,19 @@ void more_numbers(void)
 	int a = 0;
 	int b = 0;
 
-	while (b <= 9)
+	while (b <= 10)
 	{
 		while (a <= 14)
 		{
-			if (a > 9)
+			if (a >= 10)
 			{
 				_putchar(a / 10 + '0');
 				_putchar(a % 10 + '0');
+				a++;
+			}
+			else 
+			{
+				_putchar(a + '0');
 				a++;
 			}
 
