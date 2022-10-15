1cf97d9820739d77477738d92a1a700115a6e6b2
Mateo Quiros Aspron <mateoquirosaspron@gmail.com>
2022-10-15 09:53:15 -0500

    s

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+/**
+ *swap_int - swaps the value of two integrers
+ *
+ * @a: int to swap with b
+ *
+ * @b: int to swap with a
+ */
+
+void swap_int(int *a, int *b)
+{
+	*n = *a;
+	*a = *b;
+	*b = *n;
+}
