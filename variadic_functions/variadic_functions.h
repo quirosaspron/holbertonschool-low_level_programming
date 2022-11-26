f6242a385dabbe92332cf93eff4c1ea614d34014
quirosaspron <83977126+quirosaspron@users.noreply.github.com>
2022-11-26 08:52:05 -0600

    ss

---
+++ variadic_functions/variadic_functions.h
@@ -0,0 +1,13 @@
+typedef struct validTypes
+{
+	char *valid;
+	void (*f)();
+} v_types;
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+#include <stdio.h>
+#include <stdarg.h>
