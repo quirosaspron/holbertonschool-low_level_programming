6ecbe22a2540d4218ce6aeb6b6a4c3d3d02337ff
Mateo <mateoquirosaspron@gmail.com>
2023-03-03 20:02:42 -0600

    atoiiii

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,13 +1,27 @@
-#include "main.h"
-
 /**
- * _atoi - Converts a string into an integrer
+ * _atoi - Converts a string to an integer.
+ * @s: Pointer to the string to be converted.
  *
- * @s: The string to be converted
- *
+ * Return: The converted integer.
  */
-
-void _atoi(char *s)
+int _atoi(char *s)
 {
-	
+        int i, sign, num;
+
+        i = 0;
+        sign = 1;
+        num = 0;
+
+        while (s[i])
+        {
+                if (s[i] == '-')
+                        sign *= -1;
+                if (s[i] >= '0' && s[i] <= '9')
+                        num = num * 10 + (s[i] - '0');
+                if (num != 0 && !(s[i] >= '0' && s[i] <= '9'))
+                        break;
+                i++;
+        }
+
+        return (num * sign);
 }
