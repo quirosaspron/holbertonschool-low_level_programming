953afed944c7ec0d6a23976942830f81340b96ef
Mateo <mateoquirosaspron@gmail.com>
2023-03-03 20:05:51 -0600

    fixed style

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -6,22 +6,22 @@
  */
 int _atoi(char *s)
 {
-        int i, sign, num;
+	int i, sign, num;
 
-        i = 0;
-        sign = 1;
-        num = 0;
+	i = 0;
+	sign = 1;
+	num = 0;
 
-        while (s[i])
-        {
-                if (s[i] == '-')
-                        sign *= -1;
-                if (s[i] >= '0' && s[i] <= '9')
-                        num = num * 10 + (s[i] - '0');
-                if (num != 0 && !(s[i] >= '0' && s[i] <= '9'))
-                        break;
-                i++;
-        }
+	while (s[i])
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+			num = num * 10 + (s[i] - '0');
+		if (num != 0 && !(s[i] >= '0' && s[i] <= '9'))
+			break;
+		i++;
+	}
 
-        return (num * sign);
+	return (num * sign);
 }
