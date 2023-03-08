a3a42d1299e7277f31afe447b8801c65bf86e1ee
Mateo <mateoquirosaspron@gmail.com>
2023-03-07 21:02:29 -0600

    corrected style

--- static_libraries/main.h
+++ static_libraries/main.h
@@ -1,3 +1,6 @@
+#ifndef HEADER_FILE_NAME_H
+#define HEADER_FILE_NAME_H
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -37,3 +40,5 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
+#endif
