e14f68079611bdd881371803898b6fccc4adafc9
Mateo <mateoquirosaspron@gmail.com>
2023-03-08 18:58:35 -0600

    added argc use

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
+/**
+ * main - program prints its own name.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-	if(argc > 0)
+	if (argc > 0)
 	{
 		printf("%s\n", argv[0]);
 	}
-	return 0;
+	return (0);
 }
