a09f965001c27daf5bbb2dc8206d64e448670e56
Mateo <mateoquirosaspron@gmail.com>
2023-03-08 19:20:01 -0600

    a little test

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -11,10 +11,10 @@ int main(int argc, char *argv[])
 	int i;
 	(void)argv;
 
-	for (i = 0; i <= argc; i++)
+	for (i = 0; i < argc - 1; i++)
 	{
-		printf("%d\n", i);
-	} 
 
+	} 
+	printf("%d\n", i);
 	return (0);
 }
