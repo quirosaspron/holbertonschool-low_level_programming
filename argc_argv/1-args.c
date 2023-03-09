811d788e6c53b514b0dc9735c7f0f3a532d96080
Mateo <mateoquirosaspron@gmail.com>
2023-03-08 19:08:22 -0600

    a new function in the family!

---
+++ argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - program prints its own name.
+ * @argc: argument count
+ * @argv: argument vectors
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	(void)argv;
+
+	for (i = 0; i <= argc; i++)
+	{
+		printf("%d\n", i);
+	} 
+
+	return (0);
+}
