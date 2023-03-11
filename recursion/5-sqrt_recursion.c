7815ed46eff1a3e4d96001272a9d826e5cec75f0
Mateo <mateoquirosaspron@gmail.com>
2023-03-10 20:04:51 -0600

    should review this later

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -9,10 +9,21 @@
 
 int _sqrt_recursion(int n)
 {
-	if (n == 1)
+	int root;
+
+	if (n < 0)
 	{
-		return (1);
+		return (-1);
 	}
 
-	return (n - _sqrt_recursion(n - 1));
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	root = _sqrt_recursion(n / 2) * 2;
+	if (root > n / _sqrt_recursion(n / 2))
+	{
+		return (-1);
+	}
+	return (root);
 }
