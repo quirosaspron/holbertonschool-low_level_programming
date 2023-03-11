47dafea20e38798ee9d73619b479eb5ae748100b
Mateo <mateoquirosaspron@gmail.com>
2023-03-10 20:17:35 -0600

    should review this later

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -9,11 +9,26 @@
 
 int is_prime_number(int n)
 {
+	int i;
+
+	if (n <= 1)
+	{
+		return (0);
+	}
 	if (n == 2)
 	{
 		return (1);
 	}
-
-
-	return (x * _pow_recursion(x, y - 1));
+	if (n % 2 == 0)
+	{
+		return (0);
+	}
+	for (i = 3; i * i <= n; i += 2)
+	{
+		if (n % i == 0)
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
