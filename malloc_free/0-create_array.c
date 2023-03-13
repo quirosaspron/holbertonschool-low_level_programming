f6ccedd057531b5f0654e7912adcd0440d1a3b49
Mateo <mateoquirosaspron@gmail.com>
2023-03-13 17:25:41 -0600

    correction

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -19,10 +19,13 @@ char *create_array(unsigned int size, char c)
 	}
 
 	ar = malloc(sizeof(char) * size);
+	if (ar == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < size; i++)
 	{
 		ar[i] = c;
 	}
-	free(ar);
 	return (ar);
 }
