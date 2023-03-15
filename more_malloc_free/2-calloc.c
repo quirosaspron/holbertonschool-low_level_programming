689f9c5d94bc72d05f2d593ae1982407c4409bf0
Mateo <mateoquirosaspron@gmail.com>
2023-03-14 21:02:50 -0600

    allocating space for an array

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+/**
+ * malloc_checked - Allocates memory using malloc.
+ * @b: The size of the memory to be allocated.
+ * Return: Pointer to the allocated memory
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+	int i;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	arr = malloc(size * nmemb);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < nmemb; i++)
+	{
+		arr[i] = 0;
+	}
+	return (arr);
+}
