e132b114aa67e54348fae9c2f186be2569d7cdd4
Mateo <mateoquirosaspron@gmail.com>
2023-03-15 08:53:02 -0600

    arrays are cool

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+/**
+ * array_range - Creates an array of integers
+ * @min: Smallest int included in the array
+ * @max: Biggest int included in the array
+ * Return: The pointer to the newly created array
+ */
+
+int *array_range(int min, int max)
+{
+	int *arr;
+	int i, j, elements;
+
+	if (min > max)
+	{
+		return (NULL);
+	}
+	elements = max - min;
+	arr = malloc(sizeof(int) * elements);
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	j = 0;
+	for (i = min; i <= max; i++)
+	{
+		arr[j++] = i;
+	}
+	return (arr);
+}
