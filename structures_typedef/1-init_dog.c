4116d3aa6c18b848c4b85030eeda4d47d7c12cf1
Mateo <mateoquirosaspron@gmail.com>
2023-03-21 10:58:52 -0600

    initializing

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+
+/**
+ * init_dog - initializes a dog structure with the given values
+ * @d: pointer to the dog structure to initialize
+ * @name: string representing the dog's name
+ * @age: float representing the dog's age
+ * @owner: string representing the dog's owner
+ *
+ * Return: void
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
