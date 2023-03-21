9bb38f4dffe576f82e75d6facc83f182c34bc799
Mateo <mateoquirosaspron@gmail.com>
2023-03-21 14:06:25 -0600

    not fully functional but meh

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
-#include <stdio.h>
+
 /**
  * new_dog - creates a new dog
  * @name: string representing the dog's name
@@ -19,9 +19,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = strcpy(name);
+	new_dog->name = name;
 	new_dog->age = age;
-	new_dog->owner = strcpy(owner);
+	new_dog->owner = owner;
 
 	return (new_dog);
 }
