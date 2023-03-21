301af83536342a7fa00750dc762df7ebdcf4317a
Mateo <mateoquirosaspron@gmail.com>
2023-03-21 14:02:08 -0600

    added strcpy

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,5 +1,6 @@
 #include "dog.h"
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - creates a new dog
  * @name: string representing the dog's name
@@ -18,9 +19,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	new_dog->name = name;
+	new_dog->name = strcpy(name);
 	new_dog->age = age;
-	new_dog->owner = owner;
+	new_dog->owner = strcpy(owner);
 
 	return (new_dog);
 }
