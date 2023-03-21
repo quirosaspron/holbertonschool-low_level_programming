9637766ffcda4732193e69ee727af5c57d4239e2
Mateo <mateoquirosaspron@gmail.com>
2023-03-21 12:54:28 -0600

    printing dog iteration 1

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog - prints the dog structure given
+ * @d: pointer to the dog structure to print
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	printf("%s \n", d.name);
+	printf("%d \n", d.age);
+	printf("%s \n", d.owner);
+}
