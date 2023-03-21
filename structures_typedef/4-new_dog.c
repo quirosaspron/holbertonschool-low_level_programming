219255427504ae433f51e003b099824c02be6932
Mateo <mateoquirosaspron@gmail.com>
2023-03-21 13:44:59 -0600

    a new dog has been born

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+
+/**
+ * new_dog - creates a new dog
+ * @name: string representing the dog's name
+ * @age: float representing the dog's age
+ * @owner: string representing the dog's owner
+ *
+ * Return: void
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	if (d == NULL)
+		return;
+
+	dog_t new_dog.name = name;
+	dog_t new_dog.age = age;
+	dog_t new_dog.owner = owner;
+}
