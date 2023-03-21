ebbaaa5583cad9c43542385b0c83585b4797486b
Mateo <mateoquirosaspron@gmail.com>
2023-03-21 13:12:30 -0600

    corrected format

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -22,10 +22,10 @@ void print_dog(struct dog *d)
 		printf("Age: %f \n", d->age);
 	if (d->owner == NULL)
 	{
-		printf("(nil)");
+		printf("Owner: (nil)");
 	}
 	else
 	{
-		printf("Owner: %s \n", d->owner);
+		printf("Owner: %s", d->owner);
 	}
 }
