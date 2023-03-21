26a122d1a0770f2035ba835b81f1ae19e554864d
Mateo <mateoquirosaspron@gmail.com>
2023-03-21 10:36:32 -0600

    fixed style

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A new type with the name, age and owner of a dog
+ * @name: The name of the dog
+ * @age: The age of the dog
+ * @owner: The owner of the dog
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+} dog_t;
+
+#endif /* DOG_H */
