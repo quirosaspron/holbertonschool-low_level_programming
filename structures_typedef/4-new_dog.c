e53bf4465c78649f511e700423d87cca54ef18d6
Mateo <mateoquirosaspron@gmail.com>
2023-03-21 13:57:12 -0600

    creating dogs

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -11,7 +11,16 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t new_dog.name = name;
-	dog_t new_dog.age = age;
-	dog_t new_dog.owner = owner;
+	dog_t *new_dog = malloc(sizeof(dog_t));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+
+	return (new_dog);
 }
