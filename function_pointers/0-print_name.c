7aa7edcb06f47b1e5c80a58bef1db92424b3de86
Mateo <mateoquirosaspron@gmail.com>
2023-03-22 21:13:08 -0600

    just printing a name

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,10 +3,10 @@
 /**
  * print_name - prints a name
  * @name: name to print
- * @f: function to print name
+ * @f: function pointer casted to char pointer
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	_putchar(*name);
+	(*f)(*name);
 }
