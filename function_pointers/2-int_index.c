9db2b56cd5fa5eff1feab9a4d759062ba1f4a3a3
Mateo <mateoquirosaspron@gmail.com>
2023-03-23 21:55:59 -0600

    complete solution

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -16,6 +16,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 		for (i = 0; i < size; i++)
 		{
 			cmp(array[i]);
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
+	return (-1);
+	}
+	if (size <= 0)
+	{
+		return (-1);
 	}
 }
