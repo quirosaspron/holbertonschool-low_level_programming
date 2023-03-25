d8643f06fbc672843407072789ed8dc209d97a2f
Mateo <mateoquirosaspron@gmail.com>
2023-03-24 19:28:11 -0600

    we are in the printing business

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ * @separator: the string to be printed between numbers
+ * @n: counter
+ */
+
+void print_numbers(const char *separator, const unsiged int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	int param = 0;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		param = va_arg(list, int);
+		if (separator == NULL)
+		{
+			printf("%d", param);
+		}
+		else
+		{
+		printf("%s %d", sparator, param);
+		}
+	}
+	va_end(list);
+	printf("\n");
+}
