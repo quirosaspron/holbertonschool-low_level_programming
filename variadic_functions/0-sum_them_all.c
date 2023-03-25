685657dc3913c044ec5a9cc4e6cab0539fda5a09
Mateo <mateoquirosaspron@gmail.com>
2023-03-24 19:08:38 -0600

    getting the sum of n parameters

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,24 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - returns the sum of its parameters
+ * @n: counter
+ * Return: the sum of its parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	unsigned int sum = 0;
+	unsigned int num = 0;
+
+	va_start(list, n);
+	for (i = 0; i <= n; i++)
+	{
+		num = va_arg(list, unsigned int);
+		sum += num;
+	}
+	va_end(list);
+	return (sum);
+}
