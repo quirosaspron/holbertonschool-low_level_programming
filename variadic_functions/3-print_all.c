e236746b3d5c64a9dbf32a74ee67b631337f28b6
Mateo <mateoquirosaspron@gmail.com>
2023-03-24 20:08:11 -0600

    should review this later on

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,35 +6,35 @@
  */
 void print_all(const char * const format, ...)
 {
-        va_list args;
-        unsigned int i = 0;
-        char *separator = "";
+	va_list args;
+	unsigned int i = 0;
+	char *separator = "";
 
-        va_start(args, format);
+	va_start(args, format);
 
-        while (format && format[i])
-        {
-                switch (format[i])
-                {
-                        case 'c':
-                                printf("%s%c", separator, va_arg(args, int));
-                                break;
-                        case 'i':
-                                printf("%s%d", separator, va_arg(args, int));
-                                break;
-                        case 'f':
-                                printf("%s%f", separator, va_arg(args, double));
-                                break;
-                        case 's':
-                                separator = va_arg(args, char *) ? "" : "(nil)";
-                                printf("%s%s", separator, va_arg(args, char *));
-                                break;
-                }
-                separator = ", ";
-                i++;
-        }
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", separator, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(args, double));
+				break;
+			case 's':
+				separator = va_arg(args, char *) ? "" : "(nil)";
+				printf("%s%s", separator, va_arg(args, char *));
+				break;
+		}
+		separator = ", ";
+		i++;
+	}
 
-        va_end(args);
+	va_end(args);
 
-        printf("\n");
+	printf("\n");
 }
