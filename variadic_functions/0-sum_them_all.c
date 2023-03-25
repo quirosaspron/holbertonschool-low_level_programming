507e3558c67cb612af75cf12db71a75adaeefeb6
Mateo <mateoquirosaspron@gmail.com>
2023-03-24 19:11:20 -0600

    changed data type from unsigned int to int

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -9,9 +9,9 @@
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list list;
-	unsigned int i;
-	unsigned int sum = 0;
-	unsigned int num = 0;
+	int i;
+	int sum = 0;
+	int num = 0;
 
 	va_start(list, n);
 	for (i = 0; i <= n; i++)
