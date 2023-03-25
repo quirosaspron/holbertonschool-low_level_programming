4894431d002ca9ab3df1743c14b006ce37376d8a
Mateo <mateoquirosaspron@gmail.com>
2023-03-24 19:45:45 -0600

    was this too easy?

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+
+/**
+ * print_strings - prints strings
+ * @separator: the string to be printed between strings
+ * @n: counter
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list list;
+	unsigned int i;
+	char *param;
+
+	va_start(list, n);
+	for (i = 0; i < n; i++)
+	{
+		param = va_arg(list, char *);
+		if (i == 0)
+		{
+			printf("%s", param);
+		}
+		else if (separator == NULL)
+		{
+			printf("%s", param);
+		}
+		else
+		{
+		printf("%s%s", separator, param);
+		}
+	}
+	va_end(list);
+	printf("\n");
+}
