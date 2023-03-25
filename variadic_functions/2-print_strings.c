a6bd2a87ddf025f7b4a84e3b17e72907926ef05e
Mateo <mateoquirosaspron@gmail.com>
2023-03-24 19:47:42 -0600

    added an else if statement

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -25,6 +25,10 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		{
 			printf("%s", param);
 		}
+		else if (param == NULL)
+		{
+			printf("%s(nil)");
+		}
 		else
 		{
 		printf("%s%s", separator, param);
