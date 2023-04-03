6708cd250fec83ca695c90545d5469541101255d
Mateo <mateoquirosaspron@gmail.com>
2023-04-03 11:35:05 -0600

    added guards to the header

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t list
+ * Return: The number of nodes
+ * @h: List to print the elements of
+ */
+
+size_t print_list(const list_t *h)
+{
+	const list_t *current = h;
+	int count = 0;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", current->len, current->str);
+		}
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
