d833aca1de0f275dff03ae2f55cd1b4e444ad038
Mateo <mateoquirosaspron@gmail.com>
2023-04-03 11:27:30 -0600

    added an if statement in case str is null

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -13,7 +13,14 @@ size_t print_list(const list_t *h)
 
 	while (current != NULL)
 	{
-		printf("[%u] %s\n", current->len, current->str);
+		if (current->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%u] %s\n", current->len, current->str);
+		}
 		current = current->next;
 		count++;
 	}
