0bbf537f535dc65686928815cf3b3d9b1d5f4a67
Mateo <mateoquirosaspron@gmail.com>
2023-04-03 22:30:23 -0600

    testing

---
+++ singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a new node at the beginning of a list_t list
+ * Return: The address of the new element or NULL if it failed
+ * @head: the head of the linked list
+ * @str: string to be used as element of the linked list
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *new_node = malloc(sizeof(list_t));
+	int i;
+	list_t *ptr = *head;
+	
+	while (ptr != NULL)
+	{
+		ptr = ptr->next; 
+	}
+	ptr->next = new_node;
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	new_node->str = strdup(str);
+	new_node->len = i;
+	return (new_node);
+}
