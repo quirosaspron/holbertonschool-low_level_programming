6b07ce6a1e090182495424c0db8bc131fa50dc0d
Mateo <mateoquirosaspron@gmail.com>
2023-04-03 21:54:32 -0600

    added the len property to the new node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -10,12 +10,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
+	int i;
 
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
 	new_node->str = strdup(str);
+	new_node->len = i;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
