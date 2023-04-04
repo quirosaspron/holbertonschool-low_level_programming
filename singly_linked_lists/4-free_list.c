2af47a1b8ee490b88497b93afb9d5beba50e8d53
Mateo <mateoquirosaspron@gmail.com>
2023-04-03 22:46:43 -0600

    lists are free!!!

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * free_list - frees a linked list
+ * @head: the beginning of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head != NULL)
+	{
+	temp = head;
+	head = head->next;
+	free(temp->str);
+	free(temp);
+	}
+}
