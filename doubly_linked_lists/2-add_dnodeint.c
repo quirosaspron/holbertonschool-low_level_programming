b61a2a799f5c79adff44482a09e4ecec3d135ed0
Mateo <mateoquirosaspron@gmail.com>
2023-04-07 17:23:10 -0600

    added in case head is null

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -10,13 +10,21 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
 	if (new_node == NULL)
 	{
 		return (NULL);
 	}
 	new_node->n = n;
 	new_node->prev = NULL;
+	if (*head == NULL)
+	{
+	new_node->next = NULL;
+	}
+	else
+	{
 	new_node->next = *head;
+	}
 	*head = new_node;
 	return (new_node);
 }
