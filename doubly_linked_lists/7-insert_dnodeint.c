0df1ef279c675fcf4fce28c283fe6cdddbd7ae25
Mateo <mateoquirosaspron@gmail.com>
2023-04-07 17:59:10 -0600

    corrected some errors

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -16,7 +16,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	dlistint_t *ptr = new_node;
 	unsigned int i = 0;
 
-	new_node-> = n;
+	new_node->n = n;
 	while (i != idx)
 	{
 		temp->next = ptr;
