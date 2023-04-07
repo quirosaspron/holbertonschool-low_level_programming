89b55d6885c8c46aeccf978b176fead73d56255e
Mateo <mateoquirosaspron@gmail.com>
2023-04-07 17:15:16 -0600

    removed temp becouse we have **head

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
 	
-	new_node->d = n;
+	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
 	*head = new_node;
