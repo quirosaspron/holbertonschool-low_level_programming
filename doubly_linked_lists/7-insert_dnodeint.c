f108be209008360c79a403333b3ecb1fb63b1a2a
Mateo <mateoquirosaspron@gmail.com>
2023-04-07 18:04:19 -0600

    changed != to <

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -21,7 +21,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	while (i != idx)
+	while (i < idx)
 	{
 		temp->next = ptr;
 		ptr->prev = temp;
