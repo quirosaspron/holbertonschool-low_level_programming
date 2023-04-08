07087c8d6fac8cf5eeaaa0ea37bb1bbe65de000e
Mateo <mateoquirosaspron@gmail.com>
2023-04-07 18:40:01 -0600

    check this later on

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -9,54 +9,46 @@
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *temp;
-    dlistint_t *prev;
-    dlistint_t *next;
-    unsigned int i = 0;
+	dlistint_t *temp;
+	dlistint_t *prev;
+	dlistint_t *next;
+	unsigned int i = 0;
 
-    if (*head == NULL)
-    {
-        return (-1);
-    }
+	if (*head == NULL)
+	{
+		return (-1);
+	}
+	temp = *head;
+	if (index == 0)
+	{
+		*head = temp->next;
+		if (*head != NULL)
+		{
+			(*head)->prev = NULL;
+		}
+		free(temp);
+		return (1);
+	}
+	while (i < index && temp != NULL)
+	{
+		temp = temp->next;
+		i++;
+	}
 
-    temp = *head;
-
-    if (index == 0)
-    {
-        *head = temp->next;
-        if (*head != NULL)
-        {
-            (*head)->prev = NULL;
-        }
-        free(temp);
-        return (1);
-    }
-
-    while (i < index && temp != NULL)
-    {
-        temp = temp->next;
-        i++;
-    }
-
-    if (temp == NULL)
-    {
-        return (-1);
-    }
-
-    prev = temp->prev;
-    next = temp->next;
-
-    if (prev != NULL)
-    {
-        prev->next = next;
-    }
-
-    if (next != NULL)
-    {
-        next->prev = prev;
-    }
-
-    free(temp);
-
-    return (1);
+	if (temp == NULL)
+	{
+		return (-1);
+	}
+	prev = temp->prev;
+	next = temp->next;
+	if (prev != NULL)
+	{
+		prev->next = next;
+	}
+	if (next != NULL)
+	{
+		next->prev = prev;
+	}
+	free(temp);
+	return (1);
 }
