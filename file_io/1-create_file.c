942d220e4297428e013868fb44d2faee6de83c0e
Mateo <mateoquirosaspron@gmail.com>
2023-04-10 17:30:53 -0600

    added a 0 before the 6 in the file permission in open function

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		text_content = "";
 	}
-	file = open(filename, O_CREAT | O_RDWR, 600);
+	file = open(filename, O_CREAT | O_RDWR, 0600);
 	if (file == -1)
 	{
 		return (-1);
