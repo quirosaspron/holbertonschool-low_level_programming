fbee6f68fc8aa8f145392b9d9d6057489c49ccd1
Mateo <mateoquirosaspron@gmail.com>
2023-04-10 17:42:38 -0600

    changed file to filename inside truncate

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -35,7 +35,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	trn = truncate(file, count);
+	trn = truncate(filename, count);
 	if (trn == -1)
 	{
 		return (-1);
