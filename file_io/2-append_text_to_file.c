f460dd00553f5fc1f2616bfd492002b2a6fc7d6b
Mateo <mateoquirosaspron@gmail.com>
2023-04-10 19:58:16 -0600

    replaced file == 0 to access function

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL && file == 0)
+	if (text_content == NULL && access(filename, F_OK) == 0)
 	{
 		return (1);
 	}
