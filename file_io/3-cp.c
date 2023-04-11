8b132bb063f91cb15f81a82ebdf111c8b2281c6c
Mateo <mateoquirosaspron@gmail.com>
2023-04-11 00:57:12 -0600

    fixed betty style, it is kinda odd without the guard but the function can not be so long to complie with style

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,8 +16,10 @@ int main(int argc, char *argv[])
 	if (access(argv[2], F_OK) == 0)
 		dst = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (access(argv[1], F_OK) == -1 || access(argv[1], R_OK) == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
 	src = open(argv[1], O_RDONLY);
 	if (access(argv[2], F_OK) != 0)
 		dst = open(argv[2], O_WRONLY | O_CREAT, 0664);
@@ -30,8 +32,10 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	}
 	if (n == -1)
+	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
+	}
 	sc = close(src);
 	cc = close(dst);
 	if (sc == -1 || cc == -1)
