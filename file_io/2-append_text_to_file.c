7199b0fa5ca94757b2d1767cf1a3df97a4024793
Mateo <mateoquirosaspron@gmail.com>
2023-04-10 19:54:33 -0600

    changed available to file in if stament with &&

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -17,7 +17,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	available = access(filename, W_OK);
+	exists_and_can_write = access(filename, W_OK);
 	if (available == -1)
 	{
 		return (-1);
@@ -27,7 +27,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	if (text_content == NULL && available == 1)
+	if (text_content == NULL && file == 1)
 	{
 		return (1);
 	}
