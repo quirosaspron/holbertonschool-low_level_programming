228d1c7c660acda7eedfb56274f628d34bceecaf
Mateo <mateoquirosaspron@gmail.com>
2023-04-14 22:09:43 -0600

    solving task 4

---
+++ hash_tables/4-hash_table_get.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key
+ * @ht: the hash table to look into
+ * @key: the key we are looking for
+ * Return: the value associated with the element or NULL
+ */
+
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int i;
+	hash_node_t current;
+
+	i = key_index(key, ht->size);
+	if (ht->array[i] == NULL)
+	{
+		return (NULL);
+	}
+	current = ht->array[i]
+	while (strcmp(current->key, key) != 0 && current->next != NULL)
+	{
+		current = current->next;
+	}
+	if (strcmp(current->key, dup_key) == 0)
+	{
+		return (current->value);
+	}
+	return (NULL);
+}
