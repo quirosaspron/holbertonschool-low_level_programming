187d441c7b85daff169f941f87926d8900e4112f
Mateo <mateoquirosaspron@gmail.com>
2023-04-14 20:21:42 -0600

    correcting some errors

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -11,13 +11,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int i;
 	hash_node_t *current, *new_node;
-	char *dup_value;
+	char *dup_value, *dup_key;
 
 	if (*key == '\0' || key == NULL || ht == NULL || value == NULL)
 	{
 		return (0);
 	}
 	dup_value = strdup(value);
+	dup_key = strdup(key);
 	i = key_index((const unsigned char *)key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
@@ -39,9 +40,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		{
 			current = current->next;
 		}
-		if (current->next == NULL)
+		if (current->next == NULL && current->key != key)
 		{
-			current->next = new_node;
+			new_node->next = ht->array[i];
+			ht->array[i] = new_node;
 			return (1);
 		}
 		if (current->key == key)
