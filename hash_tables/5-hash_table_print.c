b0e8e03eeb86984aff8f8d4adbfacd54109eaa0e
Mateo <mateoquirosaspron@gmail.com>
2023-04-14 22:31:17 -0600

    idk if it will work

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: the hash table
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *current;
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	current = ht->array[i]
+	printf("{");
+	while( i != ht->size)
+	{
+		if (ht->array[i] != NULL)
+		{
+			while(current->next != NULL)
+			{
+				printf("'%s': '%s',", current->key, current->value);
+				current = current->next;
+			}
+			if (current->next == NULL)
+			{
+				printf("'%s': '%s'", current->key, curre    nt->value);
+			}
+		}
+		i++;
+	}
+	printf("}");
+}
