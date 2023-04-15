144ccb60693832da348adf889904b5fe8feeee7e
Mateo <mateoquirosaspron@gmail.com>
2023-04-14 19:47:07 -0600

    corrected a few things

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,14 +10,15 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 	unsigned long int i;
 
-	table = malloc(sizeof(hash_table_t) * size);
+	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 	{
 		return (NULL);
 	}
+	table->array = calloc(size, sizeof(hash_node_t *));
 	for (i = 0; i < size; i++)
 	{
-		table[i] = NULL;
+		table->array[i] = NULL;
 	}
 	return (table);
 }
