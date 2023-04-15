a2ac7e789d3abfba31865c27b603434e6974a173
Mateo <mateoquirosaspron@gmail.com>
2023-04-14 20:31:52 -0600

    fixed betty style

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -14,19 +14,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	char *dup_value, *dup_key;
 
 	if (*key == '\0' || key == NULL || ht == NULL || value == NULL)
+	{
 		return (0);
+	}
 	dup_value = strdup(value);
 	dup_key = strdup(key);
 	i = key_index((const unsigned char *)dup_key, ht->size);
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
+	{
 		return (0);
+	}
 	new_node->key = (char *)dup_key;
 	new_node->value = dup_value;
 	new_node->next = NULL;
-	if (ht->array[i] == NULL)
-		ht->array[i] = new_node;
-		return (1);
 	if (ht->array[i] != NULL)
 	{
 		current = ht->array[i];
