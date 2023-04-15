d3a6e9b213582a60934b2942666a46121363f0af
Mateo <mateoquirosaspron@gmail.com>
2023-04-14 22:16:05 -0600

    solved a small error

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -10,12 +10,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int i;
 	hash_node_t *current;
-
-	i = key_index((const unsigned char *)key, ht->size);
-	if (ht->array[i] == NULL)
+	if (ht == NULL || key == NULL)
 	{
 		return (NULL);
 	}
+	i = key_index((const unsigned char *)key, ht->size);
 	current = ht->array[i];
 	while (strcmp(current->key, key) != 0 && current->next != NULL)
 	{
